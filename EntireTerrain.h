cdebc3ced78e854b002c04779f2d81ba88423c7e
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-06-07 14:38:02 +0100

    Create EntireTerrain.h

---
+++ EntireTerrain.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FactionController.h"
+#include "EntireTerrain.generated.h"
+
+USTRUCT()
+struct FFactions
+{
+	GENERATED_USTRUCT_BODY()
+	UPROPERTY()
+		FText FactionName;
+	UPROPERTY()
+		FString ColourValue;
+	UPROPERTY()
+		FString Culture;
+	UPROPERTY()
+		float Money;
+};
+
+
+UCLASS()
+class NEAGAME_API AEntireTerrain : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEntireTerrain();
+	UPROPERTY(EditAnywhere)
+		int XSize = 80;
+	UPROPERTY(EditAnywhere)
+		int YSize = 100;
+	UPROPERTY(EditAnywhere)
+		TArray<FVector2D> ValidXY;
+	UPROPERTY()
+		TArray<int> Population; 
+	UPROPERTY()
+		TArray<float> Wealth;
+	UPROPERTY()
+		TArray<FString> Resource;
+	UPROPERTY(Editanywhere)
+		bool ResetTerrain;
+	UPROPERTY(Editanywhere)
+		FString MapMode; //These are factions, resources, wealth and populations
+	UPROPERTY(EditAnywhere)
+		UTexture2D* MapImage;
+	UPROPERTY(EditAnywhere)
+		UTexture2D* FactionImage;
+	UPROPERTY(EditAnywhere)
+		TArray<AFactionController*> Factions;
+	UPROPERTY(EditAnywhere)
+		TArray<UDecalComponent*> Decals;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UFUNCTION()
+		void FactionsAssignData();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};

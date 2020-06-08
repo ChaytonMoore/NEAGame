af6fcddb97779b53cbb3106104631815d4a83c1c
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-06-08 10:27:05 +0100

    Create PlayerView.h

---
+++ PlayerView.h
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "EntireTerrain.h"
+#include "PlayerViewer.generated.h"
+
+UCLASS()
+class NEAGAME_API APlayerViewer : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerViewer();
+	UFUNCTION()
+		void MoveRight();
+	UFUNCTION()
+		void MoveLeft();
+
+	UFUNCTION()
+		void StopMoveRight();
+	UFUNCTION()
+		void StopMoveLeft();
+
+	UFUNCTION()
+		void MoveDown();
+	UFUNCTION()
+		void MoveUp();
+
+	UFUNCTION()
+		void StopMoveDown();
+	UFUNCTION()
+		void StopMoveUp();
+
+	UFUNCTION()
+		void Shift();
+
+	UFUNCTION()
+		void StopShift();
+
+
+	UFUNCTION()
+		void MouseWheelDown();
+
+	UFUNCTION()
+		void StopMouseWheelDown();
+
+	UFUNCTION()
+		void LeftMouse();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY()
+		bool bRight = false;
+	UPROPERTY()
+		bool bLeft = false;
+	UPROPERTY()
+		bool bUp = false;
+	UPROPERTY()
+		bool bDown = false;
+	UPROPERTY()
+		bool bShiftDown = false;
+	UPROPERTY()
+		bool bMouseWheelDown = false;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};

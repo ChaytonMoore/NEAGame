3c5f700fd3b032a4e8afc1adadf190a887f728b6
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-06-09 20:04:56 +0100

    Update PlayerWidgetMediator.h

--- PlayerWidgetMediator.h
+++ PlayerWidgetMediator.h
@@ -1,50 +1,56 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+//This is for a class which will mediate between different 
 
+#pragma once
 
-#include "NEAGame//Public/PlayerWidgetMediator.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerWidgetMediator.generated.h"
 
-// Sets default values
-APlayerWidgetMediator::APlayerWidgetMediator()
+UCLASS()
+class NEAGAME_API APlayerWidgetMediator : public AActor
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-void APlayerWidgetMediator::SpawnTileUI()
-{
-	if (! bTileViewOpen)
-	{
-		TileUI = CreateWidget(GetWorld(), TileUIClass);
-		if (TileUI)
-		{
-			TileUI->AddToViewport();
-			bTileViewOpen = true;
-		}
-	}
-
-}
-
-void APlayerWidgetMediator::RemoveTileUI()
-{
-	TileUI->RemoveFromParent();
-	TileUI->Destruct();
-	bTileViewOpen = false;
-}
-
-
-
-// Called when the game starts or when spawned
-void APlayerWidgetMediator::BeginPlay()
-{
-	Super::BeginPlay();
+	GENERATED_BODY()
 	
-}
+public:	
+	// Sets default values for this actor's properties
+	APlayerWidgetMediator();
+	UPROPERTY()
+		FString Culture;
+	
+	//Since we can't use constructors to reference widgets I'll have to spawn this class to generate widgets based on references placed in the editor.
+	UPROPERTY(EditAnywhere, Category = "Default")
+		TSubclassOf<UUserWidget> TileUIClass;
 
-// Called every frame
-void APlayerWidgetMediator::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
+	UPROPERTY()
+		bool bTileViewOpen;
 
-}
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+		int Population;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int Manpower;
+
+	UPROPERTY()
+		UUserWidget* TileUI;
+
+	UPROPERTY()
+		FVector2D TilePosition;
+
+	UFUNCTION()
+		void SpawnTileUI();
+
+	UFUNCTION()
+		void RemoveTileUI();
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};

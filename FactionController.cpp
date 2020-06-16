b0e18e3692635aa9f37246b6055643f37972feba
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-06-16 21:48:35 +0100

    Update FactionController.cpp

--- FactionController.cpp
+++ FactionController.cpp
@@ -1,3 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
 #include "FactionController.h"
 
 // Sets default values
@@ -13,10 +16,30 @@ void AFactionController::BeginPlay()
 {
 	Super::BeginPlay();
 	//Right now this will just be for testing
+	for (size_t i = 0; i < OwnedTiles.Num(); i++)
+	{
+		//This will be something that finds out if the factions actually has a metal in one of its provinces.
+	}
+	//Right now it will say all factions have a metal
+	HasMetal = true;
 }
 
 //Something to do with manual tick or turn based.
 void AFactionController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+
+float RecaclulateIncome(TArray<int> Population,TArray<float> Wealth, float TaxRate)
+{
+	float Output = 0;
+	for (size_t i = 0; i < Population.Num(); i++)
+	{
+		//For each province
+		Output += Population[i] * Wealth[i] * TaxRate;
+
+	}
+
+	return Output;
 }
